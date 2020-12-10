0b24f60ec93c8d270708f248d7886b219278dd7b
Lasse Stausgaard <L.stausgaard@gmail.com>
2020-12-10 15:07:27 +0100

    service page done

---
+++ js/scriptHover.js
@@ -0,0 +1,17 @@
+/* 
+document.addEventListener("DOMContentLoaded", initPage);
+
+
+function initPage() {
+
+    let imgs = document.querySelectorAll(".hover-container img");
+
+   
+    imgs.forEach(img => {
+        console.log(img)
+        img.addEventListener("mouseover", () => {
+            img.style.zIndex = "10";
+        })
+    })
+
+}; */
